beec676e62632b195ddbbc06ff706f44ee36f1bb
mattheww227 <mattheww227@nycstudents.net>
2025-11-17 18:18:35 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,9 @@
 public class Main
 {
-
+    static void main(String[] args)
+    {
+        FrogSimulation sim = new FrogSimulation(24, 5);
+        System.out.println(sim.simulate());
+        System.out.println(sim.runSimulations(100));
+    }
 }
